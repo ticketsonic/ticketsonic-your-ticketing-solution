250d47308cd71dd84ba4549f63f9114555861d14
Martin Vassilev <mrtn.vassilev@gmail.com>
2020-06-24 22:11:44 +0300

    Workaround for the displaying of the BGN currency on a ticket.

--- woocommerce-ticketshit/includes/helper.php
+++ woocommerce-ticketshit/includes/helper.php
@@ -14,7 +14,7 @@ if (!function_exists('write_log')) {
 
 function currency_to_ascii($currency_code) {
     $currencies = array(
-        'BGN' => 'лв.',
+        'BGN' => 'BGN',
         'USD' => chr(36),
         'EUR' => chr(128),
         'GBP' => chr(163)
