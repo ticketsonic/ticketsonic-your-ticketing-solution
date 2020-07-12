e744f64a94bf93830774082efa8c2c1c5054607e
Martin Vassilev <mrtn.vassilev@gmail.com>
2020-07-12 17:51:49 +0300

    We change the api.

--- woocommerce-ticketshit/includes/bridge.php
+++ woocommerce-ticketshit/includes/bridge.php
@@ -2,13 +2,12 @@
 
 function get_ticket_data_from_origin() {
 	$url = woo_ts_get_option('ticket_info_endpoint', '');
-	$response = wp_remote_post($url, array(
+	$response = wp_remote_get($url, array(
 		'sslverify' => false,
-		'method' => 'POST',
 		'timeout' => 10,
 		'body' => array(
-			'email' => woo_ts_get_option('promoter_email', ''),
-			'api_key' => woo_ts_get_option('api_key', ''),
+			'promoter_email' => woo_ts_get_option('promoter_email', ''),
+			'promoter_api_key' => woo_ts_get_option('api_key', ''),
 		)
 	));
 
