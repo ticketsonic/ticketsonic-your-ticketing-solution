d9e135ff4b7a2fe850e8580e11e01d44bc3c33c8
Martin Vassilev <mrtn.vassilev@gmail.com>
2021-07-20 16:48:27 +0300

    Refactoring.

--- woocommerce-ticketshit/includes/helper.php
+++ woocommerce-ticketshit/includes/helper.php
@@ -163,31 +163,25 @@ function request_create_tickets_order_in_remote($order_id, $url, $email, $key) {
 		$data["start_time"] = strval(intval($appointment->timestamp));
 		$data["end_time"] = strval(intval($appointment->timestamp) + $minutes_diff * 60);
 	}
-    write_log('request_barcodes_from_ts for order ' . $order_id . ' is fired');
-    write_log('sending req to TS');
+
     $headers = array(
         "x-api-userid" => $email,
         "x-api-key" => $key
     );
     $body = prepare_order_tickets_request_body($order_id, $email, $key, $data);
-    $response = post_request_to_remote($url, $headers, $body);
 
-    write_log('result from the request to TS for ' . $order_id . ' is received');
+    $response = post_request_to_remote($url, $headers, $body);
 
     $order = wc_get_order($order_id);
     if ($response['status'] != 'success') {
-        write_log('Error fetching result for order ' . $order_id . ': '. $response['message']);
         $order->update_status('failed', 'Error fetching result for order ' . $order_id . ': '. $response['message']);
         return;
     }
-    write_log('$json_response is: ' . print_r($response, 1));
 
-    $ticket_file_paths = generate_ticket_files($response, $order_id);
-    write_log('File tickets generation for order ' . $order_id . ' is completed');
+    $ticket_file_paths = generate_ticket_files($response["tickets"], $order_id);
     
     $order->add_meta_data('ticket_file_paths', $ticket_file_paths);
     $order->save();
-    write_log('Order meta for ticket files for order ' . $order_id . ' is saved');
 
     return $order;
 }
@@ -223,15 +217,13 @@ function prepare_order_tickets_request_body($order_id, $email, $key, $data) {
     return $body;
 }
 
-function generate_ticket_files($response, $order_id) {
+function generate_ticket_files($tickets_data, $order_id) {
     // TODO: Add a check if is writable
     wp_mkdir_p(WOO_TS_UPLOADPATH . '/' . $order_id . '/');
 
     $ticket_file_paths = array();
     
-    $starttime = microtime(true);
-    foreach ($response['tickets'] as $key => $ticket) {
-        write_log('start generation of ticket file');
+    foreach ($tickets_data as $index => $ticket) {
         $decoded = decode_barcode($ticket['encrypted_data']);
         if ($decoded == null)
             return null;
@@ -239,27 +231,19 @@ function generate_ticket_files($response, $order_id) {
         $woo_product_id = wc_get_product_id_by_sku($ticket['sku']);
         $woo_product = new WC_Product_Simple($woo_product_id);
 
-        // Create separate ticket files
-        $temp = generate_file($woo_product->get_name(), $woo_product->get_description(), $decoded['formatted_price'], $decoded['sensitive_decoded'], $order_id, $key);
-        $ticket_file_paths['ticket_file_abs_path'][] = $temp['ticket_file_abs_path'];
-        $ticket_file_paths['ticket_file_url_path'][] = $temp['ticket_file_url_path'];
-        write_log('end of generation of ticket file at: ' . date('Y-m-d H:i:s'));
+        $ticket_file = generate_file($woo_product->get_name(), $woo_product->get_description(), $decoded['formatted_price'], $decoded['sensitive_decoded'], $order_id, $index);
+        $ticket_file_paths['ticket_file_abs_path'][] = $ticket_file['ticket_file_abs_path'];
+        $ticket_file_paths['ticket_file_url_path'][] = $ticket_file['ticket_file_url_path'];
     }
-
-    $endtime = microtime(true);
-    $temp = $endtime - $starttime;
-    write_log('start: ' . $starttime);
-    write_log('end: ' . $endtime);
-    write_log('time diff: ' . $temp);
     
     return $ticket_file_paths;
 }
 
-function generate_file($name, $description, $price, $sensitive_decoded, $order_id, $key) {
+function generate_file($name, $description, $price, $sensitive_decoded, $order_id, $index) {
     $file_generator = new MPDF_Generator();
 
-    $ticket_file_abs_path = WOO_TS_UPLOADPATH . '/' . $order_id . '/' . $key . '.' . $file_generator->extension; // file extension will be appended by the generator
-    $ticket_file_url_path = WOO_TS_UPLOADURLPATH . '/' . $order_id . '/' . $key . '.' . $file_generator->extension;; // file extension will be appended by the generator
+    $ticket_file_abs_path = WOO_TS_UPLOADPATH . '/' . $order_id . '/' . $index . '.' . $file_generator->extension; // file extension will be appended by the generator
+    $ticket_file_url_path = WOO_TS_UPLOADURLPATH . '/' . $order_id . '/' . $index . '.' . $file_generator->extension;; // file extension will be appended by the generator
     $file_generator->generate_ticket($name, $description, $price, $sensitive_decoded, $ticket_file_abs_path);
     
     $ticket_file_paths = array('ticket_file_url_path' => $ticket_file_url_path, 'ticket_file_abs_path' => $ticket_file_abs_path);
@@ -283,7 +267,6 @@ function decode_barcode($encrypted_data) {
 }
 
 function send_tickets_by_mail($target_user_mail, $order_id, $ticket_file_abs_paths) {
-    write_log('send_tickets_to_email_after_payment_confirmed fired');
     if (!empty($ticket_file_abs_paths)) {
         $headers = array('Content-Type: text/html; charset=UTF-8');
         $mail_sent = wp_mail($target_user_mail, woo_ts_get_option('email_subject', ''), woo_ts_get_option('email_body', ''), $headers, $ticket_file_abs_paths);
@@ -292,14 +275,6 @@ function send_tickets_by_mail($target_user_mail, $order_id, $ticket_file_abs_pat
     }
 
     return false;
-}
-
-function get_customer_name($order) {
-    return $order->get_billing_first_name() . ' ' . $order->get_billing_last_name();
-}
-    
-function get_customer_company($order) {
-    return $order->get_billing_company();;
 }
 
 function get_request_from_remote($url, $headers, $body) {
@@ -338,14 +313,22 @@ function post_request_to_remote($url, $headers, $body) {
     return $response;
 }
 
-if (!function_exists('write_log')) {
-    function write_log($log) {
-        if (true === WP_DEBUG) {
-            if (is_array($log) || is_object($log)) {
-                error_log(date('Y-m-d H:i:s') . ': ' . print_r($log, true));
-            } else {
-                error_log(date('Y-m-d H:i:s') . ': ' . $log);
-            }
+function get_customer_name($order) {
+    return $order->get_billing_first_name() . ' ' . $order->get_billing_last_name();
+}
+    
+function get_customer_company($order) {
+    return $order->get_billing_company();;
+}
+
+
+
+function write_log($log) {
+    if (true === WP_DEBUG) {
+        if (is_array($log) || is_object($log)) {
+            error_log(date('Y-m-d H:i:s') . ': ' . print_r($log, true));
+        } else {
+            error_log(date('Y-m-d H:i:s') . ': ' . $log);
         }
     }
 }
