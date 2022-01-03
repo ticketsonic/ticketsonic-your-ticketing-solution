3113bb8d6b78d841f7e9c07708848dcb59f179d1
Martin Vassilev <mrtn.vassilev@gmail.com>
2022-01-03 18:00:57 +0200

    Fix spaces after ifs and switches.

--- includes/admin.php
+++ includes/admin.php
@@ -1,22 +1,22 @@
 <?php
 // Display admin notice on screen load
 function woo_ts_admin_notice( $message = '', $priority = 'updated', $screen = '' ) {
-	if( $priority == false || $priority == '' )
+	if ( $priority == false || $priority == '' )
 		$priority = 'updated';
-	if( $message <> '' ) {
+	if ( $message <> '' ) {
 		ob_start();
 		woo_ts_admin_notice_html( $message, $priority, $screen );
 		$output = ob_get_contents();
 		ob_end_clean();
 		// Check if an existing notice is already in queue
 		$existing_notice = get_transient( WOO_TS_PREFIX . '_notice' );
-		if( $existing_notice !== false ) {
+		if ( $existing_notice !== false ) {
 			$existing_notice = base64_decode( $existing_notice );
 			$output = $existing_notice . $output;
 		}
 		$response = set_transient( WOO_TS_PREFIX . '_notice', base64_encode( $output ), MINUTE_IN_SECONDS );
 		// Check if the Transient was saved
-		if( $response !== false )
+		if ( $response !== false )
 			add_action( 'admin_notices', 'woo_ts_admin_notice_print' );
 	}
 
@@ -25,15 +25,15 @@ function woo_ts_admin_notice( $message = '', $priority = 'updated', $screen = ''
 // HTML template for admin notice
 function woo_ts_admin_notice_html( $message = '', $priority = 'updated', $screen = '' ) {
 	// Display admin notice on specific screen
-	if( !empty( $screen ) ) {
+	if ( !empty( $screen ) ) {
 
 		global $pagenow;
 
-		if( is_array( $screen ) ) {
-			if( in_array( $pagenow, $screen ) == false )
+		if ( is_array( $screen ) ) {
+			if ( in_array( $pagenow, $screen ) == false )
 				return;
 		} else {
-			if( $pagenow <> $screen )
+			if ( $pagenow <> $screen )
 				return;
 		}
 
@@ -49,7 +49,7 @@ function woo_ts_admin_notice_html( $message = '', $priority = 'updated', $screen
 function woo_ts_admin_notice_print() {
 
 	$output = get_transient( WOO_TS_PREFIX . '_notice' );
-	if( $output !== false ) {
+	if ( $output !== false ) {
 		delete_transient( WOO_TS_PREFIX . '_notice' );
 		$output = base64_decode( $output );
 		echo $output;
@@ -83,7 +83,7 @@ add_action( 'admin_menu', 'woo_ts_admin_menu', 11 );
 
 function woo_ts_enqueue_scripts( $hook ) {
 	// Simple check that WooCommerce is activated
-	if( class_exists( 'WooCommerce' ) ) {
+	if ( class_exists( 'WooCommerce' ) ) {
 		global $woocommerce;
 		// Load WooCommerce default Admin styling
 		wp_enqueue_style( 'woocommerce_admin_styles', $woocommerce->plugin_url() . '/assets/css/admin.css' );
@@ -99,16 +99,16 @@ function woo_ts_enqueue_scripts( $hook ) {
 }
 
 function woo_ts_admin_active_tab( $tab_name = null, $tab = null ) {
-	if( isset( $_GET['tab'] ) && !$tab )
+	if ( isset( $_GET['tab'] ) && !$tab )
 		$tab = $_GET['tab'];
-	else if( !isset( $_GET['tab'] ) && woo_ts_get_option( 'skip_overview', false ) )
+	else if ( !isset( $_GET['tab'] ) && woo_ts_get_option( 'skip_overview', false ) )
 		$tab = 'import';
 	else
 		$tab = 'overview';
 
 	$output = '';
-	if( isset( $tab_name ) && $tab_name ) {
-		if( $tab_name == $tab )
+	if ( isset( $tab_name ) && $tab_name ) {
+		if ( $tab_name == $tab )
 			$output = ' nav-tab-active';
 	}
 	echo $output;
@@ -119,7 +119,7 @@ function woo_ts_tab_template( $tab = '' ) {
 
 	global $import;
 
-	if (!$tab)
+	if ( ! $tab)
 		$tab = 'overview';
 
 	switch($tab) {
@@ -128,9 +128,9 @@ function woo_ts_tab_template( $tab = '' ) {
 			break;
 
 		case 'import':
-			if( isset( $_GET['import'] ) && $_GET['import'] == WOO_TS_PREFIX )
+			if ( isset( $_GET['import'] ) && $_GET['import'] == WOO_TS_PREFIX )
 				$url = 'import';
-			if( isset( $_GET['page'] ) && $_GET['page'] == WOO_TS_PREFIX )
+			if ( isset( $_GET['page'] ) && $_GET['page'] == WOO_TS_PREFIX )
 				$url = 'page';
 			break;
 
@@ -150,8 +150,8 @@ function woo_ts_tab_template( $tab = '' ) {
 			break;
 
 	}
-	if( $tab ) {
-		if( file_exists( WOO_TS_PATH . 'templates/admin/tabs-' . $tab . '.php' ) )
+	if ( $tab ) {
+		if ( file_exists( WOO_TS_PATH . 'templates/admin/tabs-' . $tab . '.php' ) )
 			include_once( WOO_TS_PATH . 'templates/admin/tabs-' . $tab . '.php' );
 	}
 
