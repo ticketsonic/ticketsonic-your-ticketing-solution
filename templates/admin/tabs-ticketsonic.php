dbe762235b7384c8459d7387442f1f5785e81816
Martin Vassilev <mrtn.vassilev@gmail.com>
2022-01-04 20:25:12 +0200

    Add/remove spaces.

--- templates/admin/tabs-ticketsonic.php
+++ templates/admin/tabs-ticketsonic.php
@@ -2,37 +2,37 @@
 
 require_once WOO_TS_PATH . '/includes/ticketsonic.php';
 
-$url = woo_ts_get_option('event_info_endpoint', '');
-if (empty($url)) {
-	woo_ts_admin_notice('Event Info Endpoint have to set in Settings', 'error');
+$url = woo_ts_get_option( 'event_info_endpoint', '' );
+if ( empty( $url ) ) {
+	woo_ts_admin_notice( 'Event Info Endpoint have to set in Settings', 'error' );
 
 	return;
 }
 
-$email = woo_ts_get_option('api_userid', '');
-if (empty($email)) {
-	woo_ts_admin_notice('Partner E-mail have to set in Settings', 'error');
+$email = woo_ts_get_option( 'api_userid', '' );
+if ( empty( $email ) ) {
+	woo_ts_admin_notice( 'Partner E-mail have to set in Settings', 'error' );
 
 return;
 }
 
-$key = woo_ts_get_option('api_key', '');
-if (empty($key)) {
-	woo_ts_admin_notice('Partner API Key have to set in Settings', 'error');
+$key = woo_ts_get_option( 'api_key', '' );
+if ( empty( $key ) ) {
+	woo_ts_admin_notice( 'Partner API Key have to set in Settings', 'error' );
 
 	return;
 }
 
-$raw_events = get_events_data_from_remote($url, $email, $key);
+$raw_events = get_events_data_from_remote( $url, $email, $key );
 
-$url = woo_ts_get_option('ticket_info_endpoint', '');
-if (empty($url)) {
-	woo_ts_admin_notice('Event Info Endpoint have to set in Settings', 'error');
+$url = woo_ts_get_option( 'ticket_info_endpoint', '' );
+if ( empty( $url ) ) {
+	woo_ts_admin_notice( 'Event Info Endpoint have to set in Settings', 'error' );
 
 	return;
 }
 
-$raw_tickets = get_event_ticket_data_from_remote($url, $email, $key, null);
+$raw_tickets = get_event_ticket_data_from_remote( $url, $email, $key, null );
 ?>
 <div class="remote-data">
 	<h3><div class="dashicons dashicons-admin-settings"></div>&nbsp;List of events</h3>
@@ -52,9 +52,9 @@ $raw_tickets = get_event_ticket_data_from_remote($url, $email, $key, null);
 				</tr>
 			</thead>    
 			<tbody>
-				<?php if (count($raw_events['events']) > 0): ?>
-					<?php foreach ($raw_events['events'] as $key => $event): ?>
-						<?php $badge_data = json_decode($event['badge_data']); ?>
+				<?php if ( count( $raw_events['events'] ) > 0 ) : ?>
+					<?php foreach ( $raw_events['events'] as $key => $event ) : ?>
+						<?php $badge_data = json_decode( $event['badge_data'] ); ?>
 						<tr id="row-<?php print $key; ?>">
 							<td class="event-id"><?php print $event['event_id']; ?></td>
 							<td class="title"><?php print $event['title']; ?></td>
@@ -89,12 +89,12 @@ $raw_tickets = get_event_ticket_data_from_remote($url, $email, $key, null);
 				</tr>
 			</thead>
 			<tbody>
-				<?php if (count($raw_tickets['tickets']) > 0): ?>
-					<?php foreach ($raw_tickets['tickets'] as $key => $ticket): ?>
+				<?php if ( count( $raw_tickets['tickets'] ) > 0 ) : ?>
+					<?php foreach ( $raw_tickets['tickets'] as $key => $ticket ) : ?>
 					<tr id="row-<?php print $key; ?>">
 						<td class="sku"><?php print $ticket['sku']; ?></td>
 						<td class="ticket-title"><?php print $ticket['primary_text_pl']; ?></td>
-						<td class="price"><?php printf('%2.2f', $ticket['price'] / 100); ?></td>
+						<td class="price"><?php printf( '%2.2f', $ticket['price'] / 100 ); ?></td>
 						<td class="currency"><?php print $ticket['currency']; ?></td>
 						<td class="stock"><?php print $ticket['stock']; ?></td>
 						<td class="event_id"><?php print $ticket['event_id']; ?></td>
