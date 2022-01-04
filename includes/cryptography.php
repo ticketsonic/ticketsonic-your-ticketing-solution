d8b62cf4923dff21283ad82e5d80bb747429ef6c
Martin Vassilev <mrtn.vassilev@gmail.com>
2022-01-04 09:51:33 +0200

    Append and prepend spaces to args and indices.

--- includes/cryptography.php
+++ includes/cryptography.php
@@ -1,89 +1,89 @@
 <?php
 
-function parse_raw_recrypted_ticket($raw_decrypted_ticket) {
+function parse_raw_recrypted_ticket( $raw_decrypted_ticket ) {
 	$result = array();
 	$checksum = 0;
 	$checksumpos = 0;
 	$i = 0;
 	try {
-		while ($i < strlen($raw_decrypted_ticket)) {
-			$label = $raw_decrypted_ticket[$i++];
-			$len = ord($raw_decrypted_ticket[$i++]);
-			switch ($label) {
+		while ( $i < strlen( $raw_decrypted_ticket ) ) {
+			$label = $raw_decrypted_ticket[ $i++ ];
+			$len = ord( $raw_decrypted_ticket[ $i++ ] );
+			switch ( $label ) {
 				case 'V':
-					$result['version'] = bin_to_int_data(substr($raw_decrypted_ticket, $i, $len));
+					$result['version'] = bin_to_int_data( substr( $raw_decrypted_ticket, $i, $len ) );
 					break;
 
 				case 'H':
-					$result['barcode'] = substr($raw_decrypted_ticket, $i, $len);
+					$result['barcode'] = substr( $raw_decrypted_ticket, $i, $len );
 					break;
 
 				case '$':
-					$result['price'] = bin_to_int_data(substr($raw_decrypted_ticket, $i, $len));
+					$result['price'] = bin_to_int_data( substr( $raw_decrypted_ticket, $i, $len ) );
 					break;
 
 				case 'I':
-					$result['product_id'] = bin_to_int_data(substr($raw_decrypted_ticket, $i, $len));
+					$result['product_id'] = bin_to_int_data( substr( $raw_decrypted_ticket, $i, $len ) );
 					break;
 
 				case 'E':
-					$result['event_id'] = bin_to_int_data(substr($raw_decrypted_ticket, $i, $len));
+					$result['event_id'] = bin_to_int_data( substr( $raw_decrypted_ticket, $i, $len ) );
 					break;
 
 				case 'S':
-					$result['segment1'] = bin_to_int_data(substr($raw_decrypted_ticket, $i, $len));
+					$result['segment1'] = bin_to_int_data( substr( $raw_decrypted_ticket, $i, $len ) );
 					break;
 
 				case 'B':
-					$result['segment2'] = bin_to_int_data(substr($raw_decrypted_ticket, $i, $len));
+					$result['segment2'] = bin_to_int_data( substr( $raw_decrypted_ticket, $i, $len ) );
 					break;
 
 				case 'R':
-					$result['segment3'] = bin_to_int_data(substr($raw_decrypted_ticket, $i, $len));
+					$result['segment3'] = bin_to_int_data( substr( $raw_decrypted_ticket, $i, $len ) );
 					break;
 
 				case 'P':
-					$result['segment4'] = bin_to_int_data(substr($raw_decrypted_ticket, $i, $len));
+					$result['segment4'] = bin_to_int_data( substr( $raw_decrypted_ticket, $i, $len ) );
 					break;
 
 				case 'T':
-					$result['start_time'] = bin_to_int_data(substr($raw_decrypted_ticket, $i, $len));
+					$result['start_time'] = bin_to_int_data( substr( $raw_decrypted_ticket, $i, $len ) );
 					break;
 
 				case 'F':
-					$result['end_time'] = bin_to_int_data(substr($raw_decrypted_ticket, $i, $len));
+					$result['end_time'] = bin_to_int_data( substr( $raw_decrypted_ticket, $i, $len ) );
 					break;
 
 				case 'X':
-					$temp = substr($raw_decrypted_ticket, $i, $len);
-					$delimiter_pos = strpos($temp, '=');
+					$temp = substr( $raw_decrypted_ticket, $i, $len );
+					$delimiter_pos = strpos( $temp, '=' );
 
-					$result['extension.' . substr($temp, 0, $delimiter_pos)] = substr($temp, $delimiter_pos + 1);
+					$result[ 'extension.' . substr( $temp, 0, $delimiter_pos ) ] = substr( $temp, $delimiter_pos + 1 );
 					break;
 
 				case 'C':
 					$checksumpos = $i - 2;
-					$checksum = bin_to_int_data(substr($raw_decrypted_ticket, $i, $len));
+					$checksum = bin_to_int_data( substr( $raw_decrypted_ticket, $i, $len ) );
 					break;
-				
+
 				default:
-					$result[$label] = substr($raw_decrypted_ticket, $i, $len);
+					$result[ $label ] = substr( $raw_decrypted_ticket, $i, $len );
 					$result['warning'] = 'Unrecognized label';
 					break;
 			}
 			$i += $len;
 		}
-	} catch (Exception $e) {
+	} catch ( Exception $e ) {
 		$result['error'] = 1;
 	}
 
-	if ($checksumpos > 0) {
+	if ( $checksumpos > 0 ) {
 		$c = 0;
-		for ($i = 0; $i < $checksumpos; $i++) {
-			$c = ($c + ord($raw_decrypted_ticket[$i])) & 0xFFFF;
+		for ( $i = 0; $i < $checksumpos; $i++ ) {
+			$c = ( $c + ord( $raw_decrypted_ticket[ $i ] ) ) & 0xFFFF;
 		}
-		
-		if ($c != $checksum) {
+
+		if ( $c != $checksum ) {
 			$result['error'] = 1;
 		}
 	}
@@ -91,31 +91,31 @@ function parse_raw_recrypted_ticket($raw_decrypted_ticket) {
 	return $result;
 }
 
-function bin_to_int_data($str) {
+function bin_to_int_data( $str ) {
 	$result = 0;
-	for ($i = strlen($str) - 1; $i >= 0; $i--) {
-		$result = ($result << 8) | ord($str[$i]);
+	for ( $i = strlen( $str ) - 1; $i >= 0; $i-- ) {
+		$result = ( $result << 8 ) | ord( $str[ $i ] );
 	}
 
 	return $result;
 }
 
-function bin_to_int_array($str) {
+function bin_to_int_array( $str ) {
 	$result = array();
-	for ($i = strlen($str) - 1; $i >= 0; $i--) {
-		$result[] = ord($str[$i]);
+	for ( $i = strlen( $str ) - 1; $i >= 0; $i-- ) {
+		$result[] = ord( $str[ $i ] );
 	}
 
 	return $result;
 }
 
-function int_array_to_bin($str) {
+function int_array_to_bin( $str ) {
 	$result = array();
-	for ($i = count($str) - 1; $i >= 0; $i--) {
-		$result[] = chr($str[$i]);
+	for ( $i = count( $str ) - 1; $i >= 0; $i-- ) {
+		$result[] = chr( $str[ $i ] );
 	}
 
-	return implode('', $result);
+	return implode( '', $result );
 }
 
 ?>
