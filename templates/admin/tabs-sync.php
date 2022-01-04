8c5a688bdc3c04deabb3d57f18226f9715315072
Martin Vassilev <mrtn.vassilev@gmail.com>
2022-01-04 20:09:41 +0200

    Misc.

--- templates/admin/tabs-sync.php
+++ templates/admin/tabs-sync.php
@@ -1,21 +1,21 @@
 <div id="poststuff">
-    <div id="remote-sync">
-        <form id="sync_with_ts" enctype="multipart/form-data" method="post">
-            <?php do_action("woo_ts_before_upload" ); ?>
+	<div id="remote-sync">
+		<form id="sync_with_ts" enctype="multipart/form-data" method="post">
+			<?php do_action( 'woo_ts_before_upload' ); ?>
 
-                <h3><div class="dashicons dashicons-admin-settings"></div>&nbsp;<?php _e( "Sync tickets with TicketSonic", "woo_ts" ); ?></h3>
-                <p class="description"><?php _e( "Sync your tickets as WooCommerce products with TicketSonic.", "woo_ts" ); ?></p>
-                <div class="inside">
-                    <p class="submit">
-                        <input type="submit" value="<?php _e("Sync with TS", "woo_ts" ); ?>" class="button-primary" />
-                    </p>
-                </div>
+				<h3><div class="dashicons dashicons-admin-settings"></div>&nbsp;<?php _e( 'Sync tickets with TicketSonic', 'woo_ts' ); ?></h3>
+				<p class="description"><?php _e( 'Sync your tickets as WooCommerce products with TicketSonic.', 'woo_ts' ); ?></p>
+				<div class="inside">
+					<p class="submit">
+						<input type="submit" value="<?php _e( 'Sync with TS', 'woo_ts' ); ?>" class="button-primary" />
+					</p>
+				</div>
 
-            <?php do_action("woo_ts_after_upload"); ?>
-            <?php do_action("woo_ts_before_options"); ?>
+			<?php do_action( 'woo_ts_after_upload' ); ?>
+			<?php do_action( 'woo_ts_before_options' ); ?>
 
-            <input type="hidden" name="action" value="sync_with_ts" />
-            <?php wp_nonce_field( "update-options" ); ?>
-        </form>
-    </div>
+			<input type="hidden" name="action" value="sync_with_ts" />
+			<?php wp_nonce_field( 'update-options' ); ?>
+		</form>
+	</div>
 </div>
