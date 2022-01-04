51cc4f2d445c110cbb499651bc9c5d7508372f09
Martin Vassilev <mrtn.vassilev@gmail.com>
2022-01-04 11:30:28 +0200

    Misc fixes.

--- includes/html_generator.php
+++ includes/html_generator.php
@@ -10,13 +10,13 @@ class HTML_Generator {
 		$this->html = woo_ts_get_option( 'email_body', '' );
 	}
 
-	function generate_file( $name, $description, $price, $sensitive_decoded, $ticket_file_abs_path ) {
+	public function generate_file( $name, $description, $price, $sensitive_decoded, $ticket_file_abs_path ) {
 		$qr = $this->get_html_qr( get_qr_matrix( base64_encode( $sensitive_decoded ) ) );
 		$this->html = str_replace( '[ticket_qr]', $qr, $this->html );
 		$this->html = str_replace( '[ticket_title]', $name, $this->html );
 		$this->html = str_replace( '[ticket_description]', $description, $this->html );
 		$this->html = str_replace( '[ticket_price]', $price, $this->html );
-		
+
 		$result = file_put_contents( $ticket_file_abs_path, $this->html );
 
 		if ( $result === false ) {
@@ -26,13 +26,13 @@ class HTML_Generator {
 		return array( 'status' => 'success' );
 	}
 
-	function get_html_qr( $data ) {
+	public function get_html_qr( $data ) {
 		$output = '<table>';
-		
-		foreach( $data as $key => $row ) {
+
+		foreach ( $data as $key => $row ) {
 			$output .= '<tr>';
 			for ( $i = 0; $i < $row->count(); $i++ )
-				if ( $row[$i] == 1 )
+				if ( $row[ $i ] == 1 )
 					$output .= '<td class="black-square"></td>';
 				else
 					$output .= '<td class="white-square"></td>';
@@ -44,4 +44,3 @@ class HTML_Generator {
 		return $output;
 	}
 }
-?>
